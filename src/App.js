bf58895e3c68a2c0738e2d6f1bf3466bcd321bf8
Nobody-123 <soapmactavice.sas@gmail.com>
2020-03-16 14:36:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
 
-function App() {
-  return (
-    <div className="App">
-      Hey
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount(){
+    const chatManager = new ChatManager({
+      instanceLocator: 'v1:us1:9928246a-b97e-4ddf-8d70-3e9d2f36bf54',
+      userId: 'diana',
+      tokenProvider: new TokenProvider({ url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/9928246a-b97e-4ddf-8d70-3e9d2f36bf54/token' })
+    })
+
+    chatManager.connect()
+      .then(currentUser => {
+        console.log('Successful connection', currentUser)
+      })
+      .catch(err => {
+        console.log('Error on connection', err)
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        Hey
+      </div>
+    );
+  }
 }
 
 export default App;
